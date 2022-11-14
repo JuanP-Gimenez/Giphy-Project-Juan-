e5e050998a813f11a1762b6dfe09402274f649f4
Juan Giménez <juan.gimenez@wundermanthompson.com>
2022-11-14 10:51:39 +0000

    hide giphy key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.giphy.com/v1/gifs/";
-export const API_KEY = "ESFZKchwnD55j6ohAVAWGjoZfub2VOno";
+export const API_KEY = "<YOUR KEY>";
 export const RES_PER_PAGE = 10;
